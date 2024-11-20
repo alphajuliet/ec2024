2cf66692d6e78e76cdae5929f907668e4bb76a56
alphajuliet <andrew@alphajuliet.com>
2024-11-20 14:27:32 +1100

    Solve q04_p1

---
+++ src/q04.clj
@@ -0,0 +1,21 @@
+(ns q04
+  (:require [util :as util]))
+
+(defn get-strikes
+  [heights]
+  (let [z (apply min heights)]
+    (->> heights
+         (map #(- % z))
+         (reduce +))))
+
+(defn part1
+  [fname]
+  (->> fname
+       util/read-data
+       (map Integer/parseInt)
+       get-strikes))
+
+(comment
+  (part1 "data/q04_p1_test.txt")
+  (part1 "data/q04_p1.txt")
+  )
