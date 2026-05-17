aefbb13ec5538362c6ecede48903512698be05a7
alphajuliet <andrew@alphajuliet.com>
2026-05-17 17:55:16 +1000

    Solve q08_p1

---
+++ src/q08.clj
@@ -0,0 +1,39 @@
+(ns q08
+  (:require [clojure.string :as str]))
+
+(defn read-data
+  [f]
+  (-> f
+      slurp
+      str/trim-newline
+      Integer/parseInt))
+      
+
+(defn part1
+  "Solution for part 1"
+  [fname]
+  (let [a (read-data fname)
+        i (-> a Math/sqrt Math/ceil int)
+        a' (* i i)
+        w (dec (* 2 i))]
+    (* w (- a' a))))
+  
+
+(defn part2
+  "Solution for part 2"
+  [fname])
+  
+
+(comment
+  (def testf1 "data/q08_p1_test.txt")
+  (def inputf1 "data/q08_p1.txt")
+
+  (def testf2 "data/q08_p1_test.txt")
+  (def inputf2 "data/q08_p2.txt")
+
+  (part1 testf1)
+  (part1 inputf1)
+
+  (part2 testf2)
+  (part2 inputf2))
+;; The End
