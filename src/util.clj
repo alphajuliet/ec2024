7e4d4a621fe96119c4b65a61c39c6fb35e65d62f
alphajuliet <andrew@alphajuliet.com>
2026-06-23 20:22:42 +1000

    Add mfind-all function to utils

--- src/util.clj
+++ src/util.clj
@@ -1,5 +1,6 @@
 (ns util
-  (:require [clojure.string :as str]))
+  (:require [clojure.core.matrix :as m]
+            [clojure.string :as str]))
 
 (defn mapmap
   "Map a function over a collection of collections"
@@ -41,5 +42,17 @@
       (if (pred (first s))
         (cons (first s) nil)
         (cons (first s) (take-until pred (rest s)))))))
+
+(defn mfind-all
+  "Return all the row and column of the given value in a matrix."
+  [m x]
+  (let [[r c] (m/shape m)]
+    (->> m
+         flatten
+         (map-indexed (fn [idx val]
+                        (if (= val x) 
+                          (vector (quot idx c) (mod idx c)) 
+                          nil)))
+         (remove nil?))))
 
 ;; The End
