c9923cb89620739d24058722df47b2021c7bb4e7
alphajuliet <andrew@alphajuliet.com>
2026-07-18 15:19:04 +1000

    Tidy up q17 code.

--- src/q17.clj
+++ src/q17.clj
@@ -2,8 +2,6 @@
   (:require [clojure.string :as str]
             [util :as util]))
 
-;; (def indices (comp range count))
-
 (defn read-data
   "Return the coordinates of all the stars in the grid."
   [f]
@@ -13,15 +11,6 @@
        (map #(str/split % #""))
        (#(util/mfind-all % "*"))))
 
-(defn find-closest-pairs
-  "For each point, find the distance to the nearest neighbour."
-  [coords]
-  (for [a coords]
-    (->> coords
-         (map (partial util/manhattan a))
-         (remove zero?)
-         (apply min))))
-
 (defn part1
   "Solution for part 1"
   [fname]
@@ -30,23 +19,15 @@
          util/mst-length
          (+ (count coords)))))
 
-(defn part2
-  "Solution for part 2"
-  [fname]
-  (let [coords (read-data fname)]
-      (->> coords
-           util/mst-length
-           (+ (count coords)))))
 (comment
   (def testf1 "data/q17_p1_test.txt")
   (def inputf1 "data/q17_p1.txt")
 
-  ;; (def testf2 "data/q17_p2_test.txt")
   (def inputf2 "data/q17_p2.txt")
  
   (part1 testf1)
   (part1 inputf1)
 
-  ;; (part2 testf2)
-  (part2 inputf2))
+  (part1 inputf2))
+
 ;; The End
